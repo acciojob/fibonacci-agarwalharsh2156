fcaa0ec1ef0ea7fdcf7630ca1d24441f6a55eef7
Harsh Agarwal <agarwalharsh2156@gmail.com>
2025-05-19 22:12:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
-// your code here
+	var a = 0;
+	var b = 1;
+	for(i=2; i<num; i++){
+		let temp = b
+		b = a+b
+		a= temp
+	}
+	return b
 }
 
 module.exports = fibonacci;
